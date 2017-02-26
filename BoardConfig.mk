436b3dbc25742e64dbf018712052391e9c888621
TheScarastic <warabhishek@gmail.com>
2017-02-26 07:56:24 +0000

    santoni: Build fs_config_files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,9 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 USE_DEVICE_SPECIFIC_GPS := true
 TARGET_NO_RPC := true
 
+# Filesystem
+TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(LOCAL_PATH)/android_filesystem_config.h
+
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
