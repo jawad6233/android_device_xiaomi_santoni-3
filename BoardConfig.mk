103da75de2ccd204f274c1923ea81ba4e83d275a
Hriday Sharma <hridaysharma42@gmail.com>
2017-06-04 16:55:07 +0530

    santoni: Update firmware on install-time

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,12 @@ TARGET_USE_SDCLANG := true
 # RIL
 TARGET_RIL_VARIANT := caf
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
+
 # Sensor
 USE_SENSOR_MULTI_HAL := true
 
